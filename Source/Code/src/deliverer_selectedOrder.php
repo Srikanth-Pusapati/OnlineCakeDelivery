c7abf825262f0d728cda84852573d284e4a165b4
anveshathmakuri <anveshathmakuri@my.unt.edu>
2016-11-15 21:00:47 -0600

    Added comments to these two files of deliverer

--- Source/Code/src/deliverer_selectedOrder.php
+++ Source/Code/src/deliverer_selectedOrder.php
@@ -4,13 +4,16 @@
 include "header.php";
 ?>
 <head>
+	<!-- This page is used to display all orders that current logined deliverer should deliver -->
 	<title>customer_orders</title>
 	<h2>customer orders to Deliver</h2>
 </head>
 <body>
 
 	<?php
-$deliverer_id=$_SESSION["userID"];   // getting deliverid from sesssion
+// getting deliverid from sesssion
+$deliverer_id=$_SESSION["userID"];  
+// These are database credentials 
 $servername = "localhost";
 $db_username = "root";
 $db_password = "";
@@ -22,13 +25,18 @@ $conn = new mysqli ( $servername, $db_username, $db_password, $dbname );
 if ($conn->connect_error) {
 	die ( "Connection failed: " . $conn->connect_error );
 }
+// This condition is used to check whether it is redirected from "deliverer_customerOrders.php"
 if(isset($_POST["orderid"]))
 {
+	//getting orderid from "deliverer_customerOrders.php" page
 	$selected_orderid= (int)$_POST["orderid"];
+	// This query is used to update deliver_id of specific selected order
 	$stmt2 = $conn->prepare("UPDATE `customer_order` SET order_status='confirmed',deliverer_id=? WHERE orderid = ?");
+	// Taking values from local declared variables
 	$stmt2->bind_param("ii", $deliverer_id,$selected_orderid);
 	$stmt2->execute();
 }
+// This query is used to display all orders that logined deliverer has to deliver
 $stmt1 = $conn->prepare(" SELECT u.user_name, o.date_of_delivery, 
 	o.time_of_delivery, o.order_mailing_address, o.city, o.zip, o.phone_no,
 	c.cake_name, c.cost_item FROM `customer_order` o JOIN `cake_details` c ON o.cakeid = c.cakeid 
@@ -36,10 +44,10 @@ $stmt1 = $conn->prepare(" SELECT u.user_name, o.date_of_delivery,
 $stmt1->bind_param("i", $deliverer_id);
 $stmt1->execute();
 $result = $stmt1->get_result();
+// this condition is used to check whether there are any orders that logined deliverer has to deliver
 if ($result->num_rows > 0) {
-// output data of each row
-//print_r($result->fetch_assoc());
 	?>
+	<!-- This form is used to display all orders that logined deliverer has to deliver in table format-->
 	<form>	
 		<table>
 			<tr>
@@ -53,6 +61,7 @@ if ($result->num_rows > 0) {
 				<th>Cost</th>
 			</tr>
 			<?php
+			// This is used to fetch each row retreived by query to get all orders that logined deliverer has to deliver
 			while ( $row = $result->fetch_assoc () ) {
 				?>
 
@@ -72,6 +81,7 @@ if ($result->num_rows > 0) {
 		<?php
 		
 	}else {
+		// this is displayed if there are no orders that logined deliverer has to deliver
 		echo "0 results";
 	}
 
