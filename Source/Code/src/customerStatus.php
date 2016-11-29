f7805932616a04831754e237fb1722e807a24444
anveshathmakuri <anveshathmakuri@my.unt.edu>
2016-11-28 22:03:34 -0600

    Update customerStatus.php
    added date of delivery

--- Source/Code/src/customerStatus.php
+++ Source/Code/src/customerStatus.php
@@ -9,14 +9,14 @@ class customerStatus extends Utils
 	 {
 		 $con= $this->connectToDatabase();
 		 $userid=$_SESSION['userID'];
-		 $sql = ("SELECT order_status FROM customer_order where userid=userid ");
+		 $sql = ("SELECT order_status,date_of_delivery,time_of_delivery FROM customer_order where userid=userid  ");
 
 						$result = $con->query( $sql);
 						if ($result->num_rows > 0) 
 						{
 							while($row = $result->fetch_assoc())
 							{
-							  echo row[order_status];
+							   echo $row["order_status"]."&emsp;&emsp;&emsp;".$row["date_of_delivery"]."&emsp;&emsp;&emsp;".$row["time_of_delivery"]."<br>";							  
 							}
 						}
 						else {
